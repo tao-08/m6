839c0028750a651dc717cf8fce76e5b5d4a1eddf
tao-08 <taoda_08@yahoo.co.jp>
2025-10-27 12:08:57 +0900

    新規登録メンバーの設定に失敗

---
+++ band_register.php
@@ -0,0 +1,84 @@
+<?php
+global $pdo;
+//DB設定
+// require_once "DB_info.php";
+// $pdo = new PDO(dsn, user, password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING)); 
+
+$n = $_POST["number"] + 1;
+
+for ($i=0; $i < $n; $i++) {
+    
+}
+
+
+
+
+for ($i=0; $i < $n; $i++) {
+    $post_band = $i."_preview_band_new";
+    $post_vocal = $i."_preview_vocal_new";
+    $post_guiter_1 = $i."_preview_guiter_1_new";
+    $post_guiter_2 = $i."_preview_guiter_2_new";
+    $post_bass = $i."_preview_bass_new";
+    $post_drum = $i."_preview_drum_new";
+    $post_keybord = $i."_preview_keybord_new";
+    $post_songs = $i."_preview_songs_new";
+
+    // $register_band = $_POST[$post_band] ?? null;
+    // $register_vocal = $_POST[$post_vocal] ?? null;
+    // $register_guiter_1 = $_POST[$post_guiter_1] ?? null;
+    // $register_guiter_2 = $_POST[$post_guiter_2] ?? null;
+    // $register_bass = $_POST[$post_bass] ?? null;
+    // $register_drum = $_POST[$post_drum] ?? null;
+    // $register_keybord = $_POST[$post_keybord] ?? null;
+    // $register_songs = $_POST[$post_songs] ?? null;
+
+    $member_register[]=[
+        $register_band = $_POST[$post_band] ?? null,
+        $register_vocal = $_POST[$post_vocal] ?? null,
+        $register_guiter_1 = $_POST[$post_guiter_1] ?? null,
+        $register_guiter_2 = $_POST[$post_guiter_2] ?? null,
+        $register_bass = $_POST[$post_bass] ?? null,
+        $register_drum = $_POST[$post_drum] ?? null,
+        $register_keybord = $_POST[$post_keybord] ?? null,
+        $register_songs = $_POST[$post_songs] ?? null
+    ];
+
+    $sql = "INSERT IGNORE into member (name) VALUE (:name)";
+    $stmt= $pdo->prepare($sql);
+    
+    foreach($member_register[$i] as $a){
+        $stmt->bindParam(":name",$a);
+    }
+        // "INSERT into band_master (id_live_detail,name,order_live,vocal_1,guiter_1,guiter_2,bass_1,drum_1,keybord_1,songs)
+        // VALUES(
+        //     :id_live_detail,
+        //     :name,
+        //     :order_live,
+        //     :vocal_1,
+        //     :guiter_1,
+        //     :guiter_2,
+        //     :bass_1,
+        //     :drum_1,
+        //     :keybord_1,
+        //     :songs
+        // );";
+
+    $stmt = $pdo->prepare($sql);
+    
+    $p = $i + 1;
+
+    // $stmt->bindParam(':id_live_detail',$p,pdo::PARAM_INT);
+    // $stmt->bindParam(':name',$register_band,pdo::PARAM_STR);
+    // $stmt->bindParam(':order_live',$p,pdo::PARAM_INT);
+    // $stmt->bindParam(':vocal_1',$register_vocal,pdo::PARAM_STR);
+    // $stmt->bindParam(':guiter_1',$register_guiter_1,pdo::PARAM_STR);
+    // $stmt->bindParam(':guiter_2',$register_guiter_2,pdo::PARAM_STR);
+    // $stmt->bindParam(':bass_1',$register_bass,pdo::PARAM_STR);
+    // $stmt->bindParam(':drum_1',$register_drum,pdo::PARAM_STR);
+    // $stmt->bindParam(':keybord_1',$register_keybord,pdo::PARAM_STR);
+    // $stmt->bindParam(':songs',$register_songs,pdo::PARAM_int);
+
+    $result = $stmt->execute();
+}
+
+?>
--- data_upload.php
+++ data_upload.php
@@ -54,36 +54,36 @@
     <h1>新規データ登録</h1>
     <div class="box">
         <form action="" method="post">
-		<div class="row">
-            <b>ライブ名</b>
-            <input type="text" name="title" class="text_input" value="<?php if(!empty($_POST["title"])){echo $_POST["title"];}?>" placeholder="◯月ライブ">
-        </div>
-		<div class="row">
-            <hr>
-            <b>列名の設定</b>
-                <table class="not_border">
-                    <tr class="label">
-                        <td>バンド名</td>
-                        <td>Vo(Gt.)</td>
-                        <td>Gt.1</td>
-                        <td>Gt.2</td>
-                        <td>Ba.</td>
-                        <td>Dr.</td>
-                        <td>Key.</td>
-                        <td>曲数</td>
-                    </tr>
-                    <tr>
-                        <td><input type="text" name="band_label" class="input_label" value="<?php echo $band_label?>"></td>
-                        <td><input type="text" name="vocal_label" class="input_label" value="<?php echo $vocal_label?>"></td>
-                        <td><input type="text" name="guiter_1_label" class="input_label" value="<?php echo $guiter_1_label?>"></td>
-                        <td><input type="text" name="guiter_2_label" class="input_label" value="<?php echo $guiter_2_label?>"></td>
-                        <td><input type="text" name="bass_label" class="input_label" value="<?php echo $bass_label?>"></td>
-                        <td><input type="text" name="drum_label" class="input_label" value="<?php echo $drum_label?>"></td>
-                        <td><input type="text" name="keybord_label" class="input_label" value="<?php echo $keybord_label?>"></td>
-                        <td><input type="text" name="songs_label" class="input_label" value="<?php echo $songs_label?>"></td>
-                    </tr>
-                </table>            
-		</div>
+            <div class="row">
+                <b>ライブ名</b>
+                <input type="text" name="title" class="text_input" value="<?php if(!empty($_POST["title"])){echo $_POST["title"];}?>" placeholder="◯月ライブ">
+            </div>
+            <div class="row">
+                <hr>
+                <b>列名の設定</b>
+                    <table class="not_border">
+                        <tr class="label">
+                            <td>バンド名</td>
+                            <td>Vo(Gt.)</td>
+                            <td>Gt.1</td>
+                            <td>Gt.2</td>
+                            <td>Ba.</td>
+                            <td>Dr.</td>
+                            <td>Key.</td>
+                            <td>曲数</td>
+                        </tr>
+                        <tr>
+                            <td><input type="text" name="band_label" class="input_label" value="<?php echo $band_label?>"></td>
+                            <td><input type="text" name="vocal_label" class="input_label" value="<?php echo $vocal_label?>"></td>
+                            <td><input type="text" name="guiter_1_label" class="input_label" value="<?php echo $guiter_1_label?>"></td>
+                            <td><input type="text" name="guiter_2_label" class="input_label" value="<?php echo $guiter_2_label?>"></td>
+                            <td><input type="text" name="bass_label" class="input_label" value="<?php echo $bass_label?>"></td>
+                            <td><input type="text" name="drum_label" class="input_label" value="<?php echo $drum_label?>"></td>
+                            <td><input type="text" name="keybord_label" class="input_label" value="<?php echo $keybord_label?>"></td>
+                            <td><input type="text" name="songs_label" class="input_label" value="<?php echo $songs_label?>"></td>
+                        </tr>
+                    </table>            
+            </div>
         </form>
 		<hr>
 		<div class="row">
@@ -129,69 +129,78 @@
             $key_songs = array_search($songs_label,$header);
             ?>
 
-            <div class="banner">インポートファイルの編集</div>
+<div class="banner">インポートファイルの編集</div>
 
-            
+
             <!-- プレビューテーブルの見出しだけ（フォームの値反映） -->
             <div>
-                <table class='table_preview'>
-                    <tr>
-                        <th><?= $band_label ?></th>
-                        <th><?= $vocal_label ?></th>
-                        <th><?= $guiter_1_label ?></th>
-                        <th><?= $guiter_2_label ?></th>
-                        <th><?= $bass_label ?></th>
-                        <th><?= $drum_label ?></th>
-                        <th><?= $keybord_label?></th>
-                        <th><?= $songs_label ?></th>
-                    </tr>
-
-                    <!-- 配列変数にバンド情報入れる -->
-                    <?php
-                    $n = 0;
-                    while(($row = fgetcsv($handle)) !== false):
-                        $band[] = [
-                            
-                            //バンド名以外空白を除去して2次元配列へ
-                            "name"=>$row[$key_band],
-                            "vocal"=>preg_replace("/[\s　]+/u","",$row[$key_vocal]),
-                            "guiter_1"=>preg_replace("/[\s　]+/u","",$row[$key_guiter_1]),
-                            "guiter_2"=>preg_replace("/[\s　]+/u","",$row[$key_guiter_2]),
-                            "bass"=>preg_replace("/[\s　]+/u","",$row[$key_bass]),
-                            "drum"=>preg_replace("/[\s　]+/u","",$row[$key_drum]),
-                            "keybord"=>preg_replace("/[\s　]+/u","",$row[$key_keybord]),
-                            "songs"=>preg_replace("/[\s　]+/u","",$row[$key_songs])
-                            
-                        ];?>
-
-                        <!-- プレビューテーブル -->
+                <form action='band_register.php' method='POST'>
+                    <table class='table_preview'>
                         <tr>
-                            <td><input placeholder="" type='text' class='band_preview' name='preview_band' value="<?= htmlspecialchars($row[$key_band],ENT_QUOTES,'UTF-8')?>"></td>
-                            <td><input placeholder="" type='text' class='text_preview' name='preview_vocal' value="<?= $band[$n]['vocal']?>"></td>
-                            <td><input placeholder="" type='text' class='text_preview' name='preview_guiter_1' value="<?= $band[$n]['guiter_1']?>"></td>
-                            <td><input placeholder="" type='text' class='text_preview' name='preview_guiter_2' value="<?= $band[$n]['guiter_2']?>"></td>
-                            <td><input placeholder="" type='text' class='text_preview' name='preview_bass' value="<?= $band[$n]['bass']?>"></td>
-                            <td><input placeholder="" type='text' class='text_preview' name='preview_drum' value="<?= $band[$n]['drum']?>"></td>
-                            <td><input placeholder="" type='text' class='text_preview' name='preview_keybord' value="<?= $band[$n]['keybord']?>"></td>
-                            <td><input placeholder="" type='number' class='songs_preview' name='preview_songs' value="<?= $band[$n]['songs']?>"></td>
+                            <th><?= $band_label ?></th>
+                            <th><?= $vocal_label ?></th>
+                            <th><?= $guiter_1_label ?></th>
+                            <th><?= $guiter_2_label ?></th>
+                            <th><?= $bass_label ?></th>
+                            <th><?= $drum_label ?></th>
+                            <th><?= $keybord_label?></th>
+                            <th><?= $songs_label ?></th>
                         </tr>
-                        <?php $n++;
-                        // var_dump($band)
-                        ?>
 
-                    <?php endwhile;?>
+                        <!-- 配列変数にバンド情報入れる -->
+                        <?php
+                        $n = 0;//nは0からなので注意
+                        while(($row = fgetcsv($handle)) !== false):
+                            $band[] = [
+                                
+                                //バンド名以外空白を除去して2次元配列へ
+                                "name"=>$row[$key_band],
+                                "vocal"=>preg_replace("/[\s　]+/u","",$row[$key_vocal]),
+                                "guiter_1"=>preg_replace("/[\s　]+/u","",$row[$key_guiter_1]),
+                                "guiter_2"=>preg_replace("/[\s　]+/u","",$row[$key_guiter_2]),
+                                "bass"=>preg_replace("/[\s　]+/u","",$row[$key_bass]),
+                                "drum"=>preg_replace("/[\s　]+/u","",$row[$key_drum]),
+                                "keybord"=>preg_replace("/[\s　]+/u","",$row[$key_keybord]),
+                                "songs"=>preg_replace("/[\s　]+/u","",$row[$key_songs])
+                                
+                            ];?>
 
-                    <!-- アップロードボタンフィールド -->
-                    <tr id="uplord_submit">
-                        <td colspan="8">
-                            <form action='' method='POST'>
-                                <input type='submit' name='preview_submit' class='preview_submit' value='アップロード'>
-                            </form>
-                        </td>
-                    </tr>
-                </table>
+                            <!-- プレビューテーブル -->
+                            <tr>
+                                <td><input type='text' class='band_preview' name='<?= $n."_"; ?>preview_band' data-master-name='<?= $n."_"; ?>preview_band' value="<?= htmlspecialchars($row[$key_band],ENT_QUOTES,'UTF-8')?>"></td>
+                                <td><input type='text' class='text_preview' name='<?= $n."_"; ?>preview_vocal' data-master-name='<?= $n."_"; ?>preview_vocal' value="<?= $band[$n]['vocal']?>"></td>
+                                <td><input type='text' class='text_preview' name='<?= $n."_"; ?>preview_guiter_1' data-master-name='<?= $n."_"; ?>preview_guiter_1' value="<?= $band[$n]['guiter_1']?>"></td>
+                                <td><input type='text' class='text_preview' name='<?= $n."_"; ?>preview_guiter_2' data-master-name='<?= $n."_"; ?>preview_guiter_2' value="<?= $band[$n]['guiter_2']?>"></td>
+                                <td><input type='text' class='text_preview' name='<?= $n."_"; ?>preview_bass' data-master-name='<?= $n."_"; ?>preview_bass' value="<?= $band[$n]['bass']?>"></td>
+                                <td><input type='text' class='text_preview' name='<?= $n."_"; ?>preview_drum' data-master-name='<?= $n."_"; ?>preview_drum' value="<?= $band[$n]['drum']?>"></td>
+                                <td><input type='text' class='text_preview' name='<?= $n."_"; ?>preview_keybord' data-master-name='<?= $n."_"; ?>preview_keybord' value="<?= $band[$n]['keybord']?>"></td>
+                                <td><input type='number' class='songs_preview' name='<?= $n."_"; ?>preview_songs' value="<?= $band[$n]['songs']?>"></td>
+                            </tr>
+                            <?php
+                            $n++;
+                            // echo $n;
+                            // var_dump($band)
+                            ?>
+
+                        <?php endwhile;?>
+
+                        <!-- アップロードボタンフィールド -->
+                        <tr id="uplord_submit">
+                            <td colspan="8">
+                                    <input type='submit' name='preview_submit' class='preview_submit' value='アップロード'>
+                                    <input type="hidden" name="number" value="<?=$n?>">
+                                    <input type="hidden" name="new" id="new" value="">
+                                </td>
+                            </tr>
+                    </table>
+                </form>
             </div>
-            
+            <?php
+            if(!empty($_POST["preview_submit"])){
+                require_once "band_register.php";
+            }?>
+
+            <?=$result;?>
         <?php endif;?>
     <?php endif;?>
 		
@@ -259,13 +268,31 @@
                 // サーバーからの結果受取
                 const result = await responce.json();
                 
-                // 結果に応じてクラス変更
+                // 結果に応じてクラスとname変更
+                // もとのname属性を保存
+                const master_name = inputElement.getAttribute("data-master-name");
+
+                // DBにいる
                 if (result.exists) {
                     inputElement.classList.add("valid");
-                    inputElement.classList.remove("invalid");                  
+                    inputElement.classList.remove("invalid");
+                    inputElement.setAttribute("name",master_name);
+
+                // DBにいない
                 }else{
                     inputElement.classList.add("invalid");
                     inputElement.classList.remove("valid");
+
+                    // const new_input = document.getElementById("new");
+                    // new_input.value = 
+                    const new_name = inputElement.getAttribute("name");
+                    if(!new_array.includes(new_name)){
+                        new_array.push(new_name);
+                    }
+
+                    // inputのname属性を変更
+                    // const new_name = master_name + "_new";
+                    // inputElement.setAttribute("name",new_name);
                 }
             }catch(error){
                 console.error("通信エラー:",error);
@@ -273,6 +300,7 @@
         };
 
         // ロード完了後
+        let new_array = [];
         document.addEventListener("DOMContentLoaded",() =>{
 
             // text_previewクラスを持つ全要素取得
