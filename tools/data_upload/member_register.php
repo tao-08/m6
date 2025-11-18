10c68df215ba56a47c0c8cd69361b9fb9a577014
tao-08 <taoda_08@yahoo.co.jp>
2025-11-18 19:49:08 +0900

    band_masterにメンバー登録する機能の経過

--- tools/data_upload/member_register.php
+++ tools/data_upload/member_register.php
@@ -11,7 +11,7 @@ $pdo = DBconnect();
 // $_POST["member"] = [0]=>[band]=>バンド名
 // 名前だけ取り出す
 array_walk_recursive($_POST["member"],
-function($value, $key) use(&$new_member, $pdo){
+function($value, $key) use(&$new_member){
     if(str_contains($key,"-new")){
         $new_member[] = $value;
     }
@@ -21,29 +21,39 @@ if(isset($new_member)){
     $sql = "INSERT into member (name) values(?)";
     $stmt = $pdo->prepare($sql);
     foreach((array)$new_member as $row){
-        $stmt->execute([$row]);
+        // $stmt->execute([$row]);
     }
 }
 
+// バンド詳細にメンバー登録
+foreach($_POST["member"] as $key=>$row){
+    $column = array_keys($row);
+    $column = str_replace("-new","",$column);
+    $member = array_combine($column,$row);
+
+	// メンバーID取得
+	$sql = "SELECT id from member WHERE name = ?";
+	$stmt = $pdo->prepare($sql);
+	foreach ($member as $key => $value) {
+		$stmt->execute([$value]);
+		$member_id[$value] = $stmt->fetch(pdo::FETCH_ASSOC);
+
+	}
 
 
-// foreach($_POST["member"] as $key=>$row){
+	// $member_id =
     
-//     $band[] = [
-//         "name"
-//         ""
-//         ""
-//         ""
-//         ""
+    // $sql = "INSERT into band_detail (name id_live_detail $key )";
+    // $stmt = $pdo->prepare($sql);
 
-//     ];
-// }
+}
 
-// print_r(
+print_r(
     // $_POST["member"],
-    // $member
-// );
+    $member
+    // $column
+);
 var_dump(
-    $new_member,
+    $member_id,
     // $result,
 );
