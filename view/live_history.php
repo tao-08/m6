d524f6d8d967673c99e899cc50891c49bfadebc2
tao-08 <taoda_08@yahoo.co.jp>
2025-11-23 23:58:43 +0900

    ID変換前プロトタイプ

--- view/live_history.php
+++ view/live_history.php
@@ -2,13 +2,27 @@
 <?php foreach ($result as $live_master_name => $row) :?>
 
 	<h2><?= $live_master_name ?></h2>
-	<table>
 		
 	<?php foreach($row as $live_detail): ?>
-		<h3><?= $live_detail["day"] ?></h3>
-		<tr>
-			
-		</tr>
+		<div class="box_1">
+			<h3><?= $live_detail["day"] ?></h3>
+			<div class="bigger"><?= $live_detail["date"] ?> 会場：<?= $live_detail["name"] ?></div>
+			<div class="">
+				<table>
+					<?php foreach($member as $live_detail_id=>$row):?>
+						<?php if($live_detail_id !== $live_detail["id_live_detail"]){continue;}?>
+						<?php foreach($row as $key=>$band):?>
+							<tr class="bigger">
+								<th><?= $band["name"] ?></th>
+								<td><?= $band["songs"] ?>曲</td>
+						<?php foreach($band as $column=>$name): if($column === "id_band"|$column === "name"|$column === "order_live"|$column === "songs"){continue;}?>
+									<td><?= $name ?></td>
+									<?php endforeach?>
+								</tr>
+						<?php endforeach?>
+					<?php endforeach?>
+				</table>
+			</div>
+		</div>
 	<?php endforeach?>
-	</table>
-<?php endforeach?>
+<?php endforeach?>;
